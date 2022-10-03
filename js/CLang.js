bb84c4d06db22c4ba37180320b8b8e2275515b01
EdArvizu <103292489+EdArvizu@users.noreply.github.com>
2022-10-03 17:29:19 -0500

    Add files via upload

---
+++ js/CLang.js
@@ -0,0 +1,28 @@
+TEXT_PRELOADER_CONTINUE = "Iniciar prueba";
+TEXT_PRELOADER_CONTINUE2 = "Preview de scratch";
+TEXT_WIN  = "YOU WIN";
+TEXT_CONGRATS = "CONGRATULATIONS!";
+TEXT_NO_WIN    = "YOU LOST YOUR CREDITS";
+TEXT_PLAY      = "PLAY";
+TEXT_HELP1     = "Tienes una oportunidad para ganar";
+TEXT_ARE_SURE = "ARE YOU SURE?";
+
+TEXT_HEAD       = "";
+TEXT_CURRENCY  = "";
+TEXT_ADVICE_WIN = "Ganaste completa el siguiente formulario para redimir tu premio";
+TEXT_ADVICE_LOSE = "Sigue intentando";
+
+TEXT_CREDIT = "Oportunidades";
+TEXT_PLUS = "+";
+TEXT_MINUS = "-";
+TEXT_BUYCARD = "Participar";
+TEXT_WIN_AMOUNT = ".";
+TEXT_CARD_VALUE = "Intentos";
+TEXT_PLAY_AGAIN = "Intentar de nuevo";
+
+TEXT_SHARE_IMAGE = "200x200.jpg";
+TEXT_SHARE_TITLE = "Congratulations!";
+TEXT_SHARE_MSG1 = "You collected <strong>";
+TEXT_SHARE_MSG2 = " points</strong>!<br><br>Share your score with your friends!";
+TEXT_SHARE_SHARE1 = "My score is ";
+TEXT_SHARE_SHARE2 = " points! Can you do better?";
